ec9f41dce549f370a673b27989150fd0775366e6
223323 <nblazic942@gmail.com>
2021-08-31 19:51:37 +0200

    allow running dumbell-lan-big with or without attackers for comparison

--- dumbbell-lan-big/experiment.sh
+++ dumbbell-lan-big/experiment.sh
@@ -6,15 +6,40 @@ export containerize_params="--pnode-type MicroCloud,dl380g3 --packing=12"
 export name='dumbell-lan-big'
 export gen_aal=gen-aal.py
 
-### NODES
-export clients=`join clientnode-{1..95}`
-export attackers=`join clientnode-{96..100}`
-export servers=`join servernode-{1..5}`
+export allow_attackers=1
 
-export tcpdump_expr=""
-export tcpdump_expr=${tcpdump_expr:4}
+if [ $allow_attackers == 0 ]; then
 
-export routers=router-1,router-2
-export some_clients=`join clientnode-{90..95}`
-export monitor=$some_clients,$routers,$attackers,$servers
-export nodes=$clients,$routers,$attackers,$servers
+	### NODES
+	#export clients=`join clientnode-{1..95}`
+	export clients=`join clientnode-{1..100}`
+	export attackers=""
+	#export attackers=`join clientnode-{96..100}`
+	export servers=`join servernode-{1..5}`
+
+	export tcpdump_expr=""
+	export tcpdump_expr=${tcpdump_expr:4}
+
+	export routers=router-1,router-2
+	export some_clients=`join clientnode-{90..95}`
+	#export monitor=$some_clients,$routers,$attackers,$servers
+	export monitor=$some_clients,$routers,$servers
+	#export nodes=$clients,$routers,$attackers,$servers
+	export nodes=$clients,$routers,$servers
+
+elif [ $allow_attackers == 1 ]; then
+
+	### NODES (with attackers)
+	export clients=`join clientnode-{1..95}`
+	export attackers=`join clientnode-{96..100}`
+	export servers=`join servernode-{1..5}`
+
+	export tcpdump_expr=""
+	export tcpdump_expr=${tcpdump_expr:4}
+
+	export routers=router-1,router-2
+	export some_clients=`join clientnode-{90..95}`
+	export monitor=$some_clients,$routers,$attackers,$servers
+	export nodes=$clients,$routers,$attackers,$servers
+
+fi
--- dumbbell-lan-big/gen-aal.py
+++ dumbbell-lan-big/gen-aal.py
@@ -2,7 +2,10 @@ import random
 clients=repl('$clients').split(',')
 monitor=repl('$monitor').split(',')
 servers=repl('$servers').split(',')
-attackers=repl('$attackers').split(',')
+
+allow_attackers=repl('$allow_attackers')
+
+
 
 
 with open('data/syn_timings.txt', 'w') as f:
@@ -12,7 +15,10 @@ with open('data/syn_timings.txt', 'w') as f:
 client_group = groups.new('client_group', clients)
 monitor_group = groups.new('monitor_group', monitor)
 server_group = groups.new('server_group', servers)
-attack_group = groups.new('attack_group', attackers)
+
+if allow_attackers == '1':
+	attackers=repl('$attackers').split(',')
+	attack_group = groups.new('attack_group', attackers)
 
 # AGENTS
 client_agent = agents.new('client_agent', client_group, '$mods/http_client/', 
@@ -21,19 +27,20 @@ monitor_agent = agents.new('monitor_agent', monitor_group, '$mods/tcpdump/tcpdum
 server_agent = agents.new('server_agent', server_group, '$mods/apache/apache.tar.gz')
 collect_agent = agents.new('collect_agent', group=monitor_group, path='$datadir/cmd.tgz',
 	execargs={'cmd': './collect.sh', 'cwd':'$expdir'})
-attack_agent = agents.new('attack_agent', group=attack_group, path='$datadir/cmd.tgz',
-	execargs={'cmd': '$datadir/syn --attack-ips 10.0.xxx.xxx --ip 10.0.1.3 '+
-		'--port 80 --threads 1 --duration 30 --attack-sleep 1000 --attack-sleep-timings $datadir/syn_timings.txt', 
-		'mark_time': 1 })
+if allow_attackers == '1':
+	attack_agent = agents.new('attack_agent', group=attack_group, path='$datadir/cmd.tgz',
+		execargs={'cmd': '$datadir/syn --attack-ips 10.0.xxx.xxx --ip 10.0.1.3 '+
+			'--port 80 --threads 1 --duration 30 --attack-sleep 1000 --attack-sleep-timings $datadir/syn_timings.txt', 
+			'mark_time': 1 })
 
-def on_off_attacks(lst):
-	for (a,b) in lst:
-		attack_agent('startCmd')
-		trigger([[a], ['clientStopped', 'stopattack']])
-		attack_agent('stopCmd')
-		trigger([[b], ['clientStopped', 'stopattack']])
-	with stream('stopattack'):
-		attack_agent('stopCmd')
+	def on_off_attacks(lst):
+		for (a,b) in lst:
+			attack_agent('startCmd')
+			trigger([[a], ['clientStopped', 'stopattack']])
+			attack_agent('stopCmd')
+			trigger([[b], ['clientStopped', 'stopattack']])
+		with stream('stopattack'):
+			attack_agent('stopCmd')
 	
 # STREAMS
 with stream('serverstream',True):
@@ -47,21 +54,23 @@ with stream('clientstream',True):
 	
 	monitor_agent('startCollection', 
 		{ 'expression': '$tcpdump_expr', 'tcpdump_args': '-z gzip -C 200' }, trigger='start_attack')
-	attack_agent('startExperiment')
+	if allow_attackers == '1':
+		attack_agent('startExperiment')
 	
 	client_agent('startClient', trigger='clientStarted')
 	trigger(500)
 	client_agent('stopClient', trigger='clientStopped')
 	
 	monitor_agent('stopCollection', trigger='cleanup')
-	
-with stream('attackstream_start',True):
-	trigger('start_attack')
-	trigger(8, 'attackstream')
-	
-	with stream('attackstream'):
-		on_off_attacks( [(5,25)] )
-		loop()
+
+if allow_attackers == '1':
+	with stream('attackstream_start',True):
+		trigger('start_attack')
+		trigger(8, 'attackstream')
+		
+		with stream('attackstream'):
+			on_off_attacks( [(5,25)] )
+			loop()
 
 with stream('cleanupstream', True):
 	trigger('cleanup')
